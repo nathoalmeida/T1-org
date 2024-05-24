af990ccd962eedb2565de6f91f5d01d9eaee9028
Nathália Zófoli <nathalia.oalmeida@gmail.com>
2024-05-24 15:12:59 -0300

    mapa registradores

--- simulador.asm
+++ simulador.asm
@@ -1,17 +1,14 @@
 # Projeto: Simulador do processador MIPS
 # Arquivo: simulador.asm
 # Descrição:
-# Autores: Maria Rita Piekas Nathália Zófoli
+# Autores: Maria Rita Piekas e Nathália Zófoli
 # data da criação: 09/04/2024
 
 .text
-# instruções
-# carregar arquivo binário na memória
-# enquanto não terminaram as instruções
-# (1) ler uma instrução
-# (2) decodificar a instrução 
-# (3) executar
-# terminar o programa
+## busca_instrucao: seta os valores de PC e IR para serem executados
+busca_instrucao:
+		
+
 
 .data
 # dados do simulador
@@ -30,4 +27,8 @@ memoria_pilha: .space 1024
 endereco_texto: .word 0x0040000000
 endereco_data: .word 0x10010000
 endereco_pilha: .word 0x7FFFEFFC
+
+## mapa dos registradores ##
+## endereço inicial: 0x10010008
+
 

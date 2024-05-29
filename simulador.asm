155ff60f57cbf8c80f4dc049e610c2cda0547cfc
Nathália Zófoli <nathalia.oalmeida@gmail.com>
2024-05-29 09:26:46 -0300

    leitura arquivo com loop, funcionando ok

--- simulador.asm
+++ simulador.asm
@@ -7,7 +7,6 @@
 .text
 ##############################################
 aberturaArquivo:
-##############################################
 	la $a0, local_arquivo 	  # carrega o endereço do arquivo em $a0
 	li $a1, 0 		  # seta a flag do arquivo em 0 para modo de leitura
 	la $a0, local_arquivo 	  # carrega o nome do arquivo em $a0
@@ -20,19 +19,30 @@ aberturaArquivo:
 	add $t1, $zero, $t0	 # armazena em $t1 o valor de retorno da chamada 
 	bltz $t1, encerraPrograma # se o retorno da chamada for negativo, houve erro na abertura do arquivo
 	
-LeituraArquivo:	
 	lw $a0, 0($t0)		# armazena o valor do descritor do arquivo em $a0 
+	
+leituraArquivo:	
 	la $a1, buffer_leitura  # carrega em $a1 o endereço da variável buffer_leitura
 	li $a2, 4		# carrega em $a2 o número de bytes que serão lidos do arquivo
 	li $v0, 14		# carrega em $v0 a chamada que será feita ao sistema (14 = leitura arquivo)
 	syscall
 	
 ## Armazena o valor de buffer_leitura para memoria_text e incrementa o endereço de memoria_text
-	la $a2, memoria_text	# carrega o endereço da variável memoria_text em $a2
+	lw $a2, endereco_texto	# carrega o endereço inicial de memoria_texto em $a2
 	lw $a3, 0($a1) 		# carrega o valor contido no buffer_leitura em $a3
-	sw $a3, 0($a2)		# armazena o valor do buffer (tá em $a1) para o endereço de memoria_text
-				## INSERIR UMA VARIÁVEL QUE INCREMENTE O ENDEREÇO DA MEMÓRIA?
+	sw $a3, 0($a2)		# armazena o valor do buffer (endereço tá em $a1) para o endereço de memoria_text
 	
+	la $s0, endereco_texto # carrega o endereço da variável endereco_texto
+	addi $t1, $a2, 4	# incrementa 4 bytes no endereço de memoria
+	sw $t1, 0($s0)		# armazena o novo valor do endereco_texto
+## DO-WHILE:
+	bgtz $v0, leituraArquivo # se o número de caracteres lidos for maior que 0, continua a leitura do arquivo
+	
+fechaArquivo:
+	li $v0, 16
+	syscall
+
+##############################################
 encerraPrograma:
 	li $v0, 17
 	syscall			# faz a chamada 17 e encerra o programa
@@ -46,7 +56,7 @@ encerraPrograma:
 	memoria_text: .space 1024
 	memoria_data: .space 1024
 	memoria_pilha: .space 1024
-	endereco_texto: .word 0x00400000
+	endereco_texto: .word 0x1001008c
 	endereco_data: .word 0x10010000
 	endereco_pilha: .word 0x7FFFEFFC
 	local_arquivo: .asciiz "/home/nathizofoli/Documentos/workspace/T1-org/trabalho_01-2024_1.bin"
